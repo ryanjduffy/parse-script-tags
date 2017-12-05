5575608c1bcbbd200ddf7b7cad8665a17a457081
Ryan Duffy <ryan@tiqtech.com>
2017-12-05 14:13:13 -0800

    Replace parse5 with custom fragment parser (Fix #6) (#7)
    * 0.1.4
    
    * 0.1.4
    
    * replace parse5 with simpler, custom fragment parser
    
    * remove parse5 from dependencies

--- index.js
+++ index.js
@@ -1,68 +1,14 @@
-const babylon = require("babylon");
-const parse5 = require("parse5");
-const types = require("babel-types");
 
-const startScript = /<script[^>]*>/im;
-const endScript = /<\/script\s*>/im;
-// https://stackoverflow.com/questions/5034781/js-regex-to-split-by-line#comment5633979_5035005
-const newLines = /\r\n|[\n\v\f\r\x85\u2028\u2029]/;
+import * as types from "babel-types";
+import * as babylon from "babylon";
 
-function getType (tag) {
-  const fragment = parse5.parseFragment(tag);
+import {
+  generateWhitespace,
+  getCandidateScriptLocations,
+  parseScripts as customParseScripts,
+  parseScriptTags as customParseScriptTags
+} from "./customParse.js";
 
-  if (fragment) {
-    const script = fragment.childNodes
-      .filter(node => node.tagName === "script")
-      .pop();
-
-    if (script) {
-      const type = script.attrs
-        .filter(attr => attr.name === "type")
-        .map(attr => attr.value)
-        .pop();
-
-      return type ? type.toLowerCase() : null;
-    }
-  }
-
-  return null;
-}
-
-function getCandidateScriptLocations(source, index) {
-  const i = index || 0;
-  const str = source.substring(i);
-
-  const startMatch = startScript.exec(str);
-  if (startMatch) {
-    const startsAt = startMatch.index + startMatch[0].length;
-    const afterStart = str.substring(startsAt);
-    const endMatch = endScript.exec(afterStart);
-    if (endMatch) {
-      const locLength = endMatch.index;
-      const locIndex = i + startsAt;
-      const endIndex = locIndex + locLength + endMatch[0].length;
-
-      // extract the complete tag (incl start and end tags and content). if the
-      // type is invalid (= not JS), skip this tag and continue
-      const tag = source.substring(i + startMatch.index, endIndex);
-      const type = getType(tag);
-      if (type && (type !== "javascript" && type !== "text/javascript")) {
-        return getCandidateScriptLocations(source, endIndex);
-      }
-
-      return [
-        adjustForLineAndColumn(source, {
-          index: locIndex,
-          length: locLength,
-          source: source.substring(locIndex, locIndex + locLength)
-        }),
-        ...getCandidateScriptLocations(source, endIndex)
-      ];
-    }
-  }
-
-  return [];
-}
 
 function parseScript({source, line}) {
   // remove empty or only whitespace scripts
@@ -80,74 +26,8 @@ function parseScript({source, line}) {
   }
 }
 
-function parseScripts(locations, parser = parseScript) {
-  return locations.map(parser);
-}
-
-function generateWhitespace(length) {
-  return Array.from(new Array(length + 1)).join(" ");
-}
-
-function calcLineAndColumn(source, index) {
-  const lines = source
-    .substring(0, index)
-    .split(newLines)
-  const line = lines.length;
-  const column = lines.pop().length + 1;
-
-  return {
-    column,
-    line
-  };
-}
-
-function adjustForLineAndColumn(fullSource, location) {
-  const {column, line} = calcLineAndColumn(fullSource, location.index);
-  return Object.assign({}, location, {
-    line,
-    column,
-    // prepend whitespace for scripts that do not start on the first column
-    source: generateWhitespace(column) + location.source
-  });
-}
-
-function parseScriptTags(source, parser) {
-  const scripts = parseScripts(
-    getCandidateScriptLocations(source),
-    parser
-  ).filter(
-    types.isFile
-  ).reduce((main, script) => {
-    return {
-      statements: main.statements.concat(script.program.body),
-      comments: main.comments.concat(script.comments),
-      tokens: main.tokens.concat(script.tokens)
-    };
-  }, {
-    statements: [],
-    comments: [],
-    tokens: []
-  });
-
-  const program = types.program(scripts.statements);
-  const file = types.file(
-    program,
-    scripts.comments,
-    scripts.tokens
-  );
-
-  const end = calcLineAndColumn(source, source.length);
-  file.start = program.start = 0;
-  file.end = program.end = source.length;
-  file.loc = program.loc = {
-    start: {
-      line: 1,
-      column: 0
-    },
-    end
-  }
-
-  return file;
+function parseScripts (locations, parser = parseScript) {
+  return customParseScripts(locations, parser);
 }
 
 function extractScriptTags(source) {
@@ -166,6 +46,11 @@ function extractScriptTags(source) {
     types.isFile
   );
 }
+
+function parseScriptTags (source, parser = parseScript) {
+  return customParseScriptTags(source, parser);
+}
+
 
 export default parseScriptTags;
 export {
